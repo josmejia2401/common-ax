da4bd4e030013d37d6b2b6393014e17cbf3438d8
jose <josmejia.2401@gmail.com>
2022-02-14 14:07:15 -0500

    Ajustes y mejoras

--- dist/src/main/index.js
+++ dist/src/main/index.js
@@ -1,31 +1,29 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ObjectValidation = exports.GeneralValidation = exports.TokenUtil = exports.RandomUtil = exports.GeneralUtil = exports.DateUtil = exports.CustomError = exports.ScopeInjector = exports.Injector = exports.OperationDDL = exports.DynamoDb = exports.BaseModel = void 0;
-var base_dynamo_model_1 = require("./db/aws/base.dynamo.model");
-Object.defineProperty(exports, "BaseModel", { enumerable: true, get: function () { return base_dynamo_model_1.BaseModel; } });
-var dynamo_db_1 = require("./db/aws/dynamo.db");
-Object.defineProperty(exports, "DynamoDb", { enumerable: true, get: function () { return dynamo_db_1.DynamoDb; } });
-var operations_db_1 = require("./db/aws/operations.db");
-Object.defineProperty(exports, "OperationDDL", { enumerable: true, get: function () { return operations_db_1.OperationDDL; } });
+__exportStar(require("./db/db"), exports);
+__exportStar(require("./db/aws/base.dynamo.model"), exports);
+__exportStar(require("./db/aws/dynamo.db"), exports);
+__exportStar(require("./db/aws/operations.db"), exports);
 //Dependency injection
-var di_1 = require("./di/di");
-Object.defineProperty(exports, "Injector", { enumerable: true, get: function () { return di_1.Injector; } });
-Object.defineProperty(exports, "ScopeInjector", { enumerable: true, get: function () { return di_1.ScopeInjector; } });
+__exportStar(require("./di/di"), exports);
 //Exceptions
-var custom_error_1 = require("./exceptions/custom-error");
-Object.defineProperty(exports, "CustomError", { enumerable: true, get: function () { return custom_error_1.CustomError; } });
+__exportStar(require("./exceptions/custom-error"), exports);
 //Utils
-var date_utils_1 = require("./utils/date.utils");
-Object.defineProperty(exports, "DateUtil", { enumerable: true, get: function () { return date_utils_1.DateUtil; } });
-var general_utils_1 = require("./utils/general.utils");
-Object.defineProperty(exports, "GeneralUtil", { enumerable: true, get: function () { return general_utils_1.GeneralUtil; } });
-var random_utils_1 = require("./utils/random.utils");
-Object.defineProperty(exports, "RandomUtil", { enumerable: true, get: function () { return random_utils_1.RandomUtil; } });
-var token_utils_1 = require("./utils/token.utils");
-Object.defineProperty(exports, "TokenUtil", { enumerable: true, get: function () { return token_utils_1.TokenUtil; } });
+__exportStar(require("./utils/date.utils"), exports);
+__exportStar(require("./utils/general.utils"), exports);
+__exportStar(require("./utils/random.utils"), exports);
+__exportStar(require("./utils/token.utils"), exports);
 //Validations
-var general_validations_1 = require("./validations/general.validations");
-Object.defineProperty(exports, "GeneralValidation", { enumerable: true, get: function () { return general_validations_1.GeneralValidation; } });
-var object_validations_1 = require("./validations/object.validations");
-Object.defineProperty(exports, "ObjectValidation", { enumerable: true, get: function () { return object_validations_1.ObjectValidation; } });
+__exportStar(require("./validations/general.validations"), exports);
+__exportStar(require("./validations/object.validations"), exports);
 //# sourceMappingURL=index.js.map
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,16 +1,16 @@
-export { BaseDB } from './db/db';
-export { BaseModel, MetadataDynamoResponse } from './db/aws/base.dynamo.model';
-export { DynamoDb } from './db/aws/dynamo.db';
-export { OperationDDL } from './db/aws/operations.db';
+export * from './db/db';
+export * from './db/aws/base.dynamo.model';
+export * from './db/aws/dynamo.db';
+export * from './db/aws/operations.db';
 //Dependency injection
-export { Injector, ScopeInjector } from './di/di';
+export * from './di/di';
 //Exceptions
-export { CustomError } from './exceptions/custom-error';
+export * from './exceptions/custom-error';
 //Utils
-export { DateUtil } from './utils/date.utils';
-export { GeneralUtil } from './utils/general.utils';
-export { RandomUtil } from './utils/random.utils';
-export { TokenUtil } from './utils/token.utils';
+export * from './utils/date.utils';
+export * from './utils/general.utils';
+export * from './utils/random.utils';
+export * from './utils/token.utils';
 //Validations
-export { GeneralValidation } from './validations/general.validations';
-export { ObjectValidation } from './validations/object.validations';
+export * from './validations/general.validations';
+export * from './validations/object.validations';
